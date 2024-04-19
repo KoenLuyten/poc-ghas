6e71abad6cdebce52f0750d6e5420ee0aea4ee78
Koen Luyten <koenluyten@gmail.com>
2024-04-19 10:38:11 +0200

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,4 +6,9 @@ var class1 = new Class1();
 var secret = "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";
 
 Console.WriteLine(class1.GetMessage());
+
+Console.WriteLine("bla");
+
+var secret2 = "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";
+
 Console.ReadKey();
